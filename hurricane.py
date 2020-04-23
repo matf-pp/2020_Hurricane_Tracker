f17ace3979ba847c85e60d92dc0a6aefebf4f62b
milenavidic <62108616+milenavidic@users.noreply.github.com>
2020-04-23 20:20:51 +0200

    Pocetna verzija
    Izabrani uragani i mapa koja im odgovara, postavljena pozadina i koordinatni sistem, napisana funkcija za ucitavanje podataka iz csv fajla i uradjeno osnovno iscrtavanje putanje na jednom primeru (bez menjanja brzine i boje).

---
+++ hurricane.py
@@ -0,0 +1,62 @@
+import turtle
+import csv
+import tkinter
+
+screen=turtle.Screen()
+screen.setup(1200,646) #velicina ekrana postavljena na velicinu slike
+screen.setworldcoordinates(-105,5,-20,45)  #podesen koordinatni sistem
+screen.title("Hurricane Katrina")
+#screen.bgpic("mapa1.gif")
+canvas=screen.getcanvas()
+#canvas.itemconfig(screen.bgpic)
+mapa=tkinter.PhotoImage(file="mapa1.gif")
+canvas.create_image(-855,-420,image=mapa)
+
+hurricane=turtle.Turtle()
+screen.register_shape("icon.gif")
+hurricane.shape("icon.gif")
+
+latitude=[]
+longitude=[]
+speed=[]
+
+def ucitaj_csv(fajl):
+    with open(fajl,"r") as csvfile:
+        reader=csv.reader(csvfile,delimiter='\t')
+        next(reader) #preskace se header
+        for row in reader:
+            latitude.append(row[2])
+            longitude.append(row[3])
+            speed.append(row[4])
+
+
+ucitaj_csv("katrina.csv")
+#print(longitude)
+
+n=len(latitude)
+
+hurricane.hideturtle()
+hurricane.penup()
+xstart=float(longitude[0])
+ystart=float(latitude[0])
+#print(xstart,ystart)
+hurricane.setx(xstart)
+hurricane.sety(ystart)
+brzina=float(speed[0])
+#hurricane.speed(brzina)
+
+hurricane.showturtle()
+hurricane.pendown()
+hurricane.color("red")
+hurricane.pensize(3)
+for i in range(1,n):
+    x=float(longitude[i])
+    y=float(latitude[i])
+    brzina=float(speed[i])
+    hurricane.setx(x)
+    hurricane.sety(y)
+    #hurricane.speed(brzina)
+
+
+
+screen.mainloop()
