574755a6fd4037a163ebebf8f242dbff65aeacf9
milenavidic <62108616+milenavidic@users.noreply.github.com>
2020-05-09 17:51:49 +0200

    GUI
    Dodat gui za koji je korišćen kivy. Uragan se selektuje, i animacija se pokreće klikom na dugme.

---
+++ gui.py
@@ -0,0 +1,30 @@
+import kivy
+import hurricane
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout 
+from kivy.core.window import Window
+
+Window.size = (1120, 630)
+
+filename="katrina.csv"
+
+
+class kvBL(BoxLayout):
+    def checkbox_click(self, instance, value):
+        pass
+    def pokreni(self):
+        hurricane.obradi_uragan(filename) 
+    def setfilename(self,text):
+        global filename
+        filename=text
+class hurricaneApp(App):
+    def build(self):
+        return kvBL()
+
+
+root = hurricaneApp()
+
+if __name__ == '__main__':
+    hurricaneApp().run()
+    
