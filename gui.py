a9bf9acfd1b1af6a262cd793205650ad42c1335f
milenavidic <62108616+milenavidic@users.noreply.github.com>
2020-05-17 23:47:56 +0200

    Update gui.py
    Animacija se ne pokrece ako nije selektovan nijedan uragan

--- gui.py
+++ gui.py
@@ -7,14 +7,15 @@ from kivy.core.window import Window
 
 Window.size = (1120, 630)
 
-filename="katrina.csv"
+filename="null"
 
 
 class kvBL(BoxLayout):
     def checkbox_click(self, instance, value):
         pass
     def pokreni(self):
-        hurricane.obradi_uragan(filename) 
+        if (filename!="null"):
+            hurricane.obradi_uragan(filename) 
     def setfilename(self,text):
         global filename
         filename=text
